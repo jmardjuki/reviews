6ecf09fdeabcfb13821135e23a3ca5a416155e8e
Janet Mardjuki <mardjuki@sfu.ca>
2018-07-13 23:16:11 -0700

    Added makefile

---
+++ operatingSystems/system/system.c
@@ -0,0 +1,15 @@
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t sysPid;
+    // Fork a new process
+    pid = fork(void);
+    
+    if (pid == -1)
+        print("Failed\n")
+    else if ( sysPid == )
+
+    return 0;
+}
